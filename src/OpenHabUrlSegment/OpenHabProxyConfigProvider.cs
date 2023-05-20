81175f31b3d7ece8f37dfd051c07119720493e16
mu88 <4560672+mu88@users.noreply.github.com>
2023-05-20 16:16:39 +0200

    wip2

--- src/OpenHabUrlSegment/OpenHabProxyConfigProvider.cs
+++ src/OpenHabUrlSegment/OpenHabProxyConfigProvider.cs
@@ -19,24 +19,6 @@ internal class OpenHabProxyConfigProvider : IProxyConfigProvider
         {
             new RouteConfig { RouteId = "route1", ClusterId = "cluster1", Match = new RouteMatch { Path = $"{config.UrlPathSegment}/{{**catch-all}}" } }
                 .WithTransformPathRemovePrefix("/openhab")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route2", ClusterId = "cluster2", Match = new RouteMatch { Path = "js/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/js")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route3", ClusterId = "cluster3", Match = new RouteMatch { Path = "css/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/css")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route4", ClusterId = "cluster4", Match = new RouteMatch { Path = "res/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/res")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route5", ClusterId = "cluster5", Match = new RouteMatch { Path = "rest/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/rest")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route6", ClusterId = "cluster6", Match = new RouteMatch { Path = "fonts/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/fonts")
-                .WithTransformRequestHeader("Host", $"{openHab}", false),
-            new RouteConfig { RouteId = "route7", ClusterId = "cluster7", Match = new RouteMatch { Path = "images/{**catch-all}" } }
-                .WithTransformPathRemovePrefix("/images")
                 .WithTransformRequestHeader("Host", $"{openHab}", false)
         };
 
@@ -46,36 +28,6 @@ internal class OpenHabProxyConfigProvider : IProxyConfigProvider
             {
                 ClusterId = "cluster1",
                 Destinations = new Dictionary<string, DestinationConfig> { { "destination1", new DestinationConfig { Address = $"http://{openHab}/" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster2",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination2", new DestinationConfig { Address = $"http://{openHab}/js" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster3",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination3", new DestinationConfig { Address = $"http://{openHab}/css" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster4",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination4", new DestinationConfig { Address = $"http://{openHab}/res" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster5",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination5", new DestinationConfig { Address = $"http://{openHab}/rest" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster6",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination6", new DestinationConfig { Address = $"http://{openHab}/fonts" } } }
-            },
-            new ClusterConfig
-            {
-                ClusterId = "cluster7",
-                Destinations = new Dictionary<string, DestinationConfig> { { "destination7", new DestinationConfig { Address = $"http://{openHab}/images" } } }
             }
         };
     }
--- src/OpenHabUrlSegment/Program.cs
+++ src/OpenHabUrlSegment/Program.cs
@@ -1,8 +1,68 @@
+using System.IO.Compression;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
 using OpenHabUrlSegment;
 using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Transforms;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy();
+builder.Services.AddReverseProxy()
+    .AddTransforms(context =>
+    {
+        context.AddResponseTransform(async responseContext =>
+        {
+            if (responseContext.ProxyResponse != null)
+            {
+                if (responseContext.ProxyResponse.Content.Headers.ContentEncoding.Contains("br"))
+                {
+                    var readAsStreamAsync = await responseContext.ProxyResponse.Content.ReadAsStreamAsync();
+                    var brotliDecompressionStream = new BrotliStream(readAsStreamAsync, CompressionMode.Decompress);
+                    using var reader = new StreamReader(brotliDecompressionStream);
+                    var body = await reader.ReadToEndAsync();
+
+                    if (!string.IsNullOrEmpty(body))
+                    {
+                        responseContext.SuppressResponseBody = true;
+
+                        var uncompressedBytes = Encoding.UTF8.GetBytes(body
+                                                                           .Replace("""src="/js/""", """src="/openhab/js/""")
+                                                                           .Replace("""href="/css/""", """href="/openhab/css/""")
+                                                                           .Replace("""href="/res/""", """href="/openhab/res/""")
+                                                                           .Replace("""window.location="/auth?response_type=code&client_id="+encodeURIComponent(window.location.origin)+"&redirect_uri="+encodeURIComponent(window.location.origin)+""", """window.location="/openhab/auth?response_type=code&client_id="+encodeURIComponent(window.location.origin)+"&redirect_uri="+encodeURIComponent(window.location.origin)+"/openhab"+""")
+                                                                           .Replace("""action="/auth""", """action="/openhab/auth""")
+                                                                           .Replace("""+"images/""", """+"openhab/images/""")
+                                                                           .Replace("""+"js/""", """+"openhab/js/""")
+                                                                           .Replace("""path:"/analyzer/""", """path:"/openhab/analyzer/""")
+                                                                           .Replace("""path:"/res/""", """path:"/openhab/res/""")
+                                                                           .Replace("""("/rest/""", """("/openhab/rest/"""));
+                        var compressedBytes = new byte[uncompressedBytes.Length];
+                        BrotliEncoder.TryCompress(uncompressedBytes, compressedBytes, out var bytesWritten);
+                        responseContext.HttpContext.Response.ContentLength = bytesWritten;
+                        await responseContext.HttpContext.Response.Body.WriteAsync(compressedBytes);
+                    }
+                }
+                else if (responseContext.ProxyResponse.Content.Headers.ContentType?.MediaType == MediaTypeNames.Text.Html)
+                {
+                    var stream = await responseContext.ProxyResponse.Content.ReadAsStreamAsync();
+                    using var reader = new StreamReader(stream);
+                    var body = await reader.ReadToEndAsync();
+                    if (!string.IsNullOrEmpty(body))
+                    {
+                        responseContext.SuppressResponseBody = true;
+
+                        var bytes = Encoding.UTF8.GetBytes(body
+                                                               .Replace("""action="/auth""", """action="/openhab/auth""")
+                                                               .Replace("""type="button" href="/">""", """type="button" href="openhab/">""")
+                                                               .Replace("""href="res/""", """href="openhab/res/""")
+                                                               .Replace("""href="/manifest.json""", """href="openhab/manifest.json"""));
+                        responseContext.HttpContext.Response.ContentLength = bytes.Length;
+                        await responseContext.HttpContext.Response.Body.WriteAsync(bytes);
+                    }
+                }
+            }
+        });
+    });
 builder.Services.AddSingleton<IProxyConfigProvider, OpenHabProxyConfigProvider>();
 builder.Services.Configure<OpenHabProxyOptions>(builder.Configuration.GetSection(OpenHabProxyOptions.SectionName));
 
